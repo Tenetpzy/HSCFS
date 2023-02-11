213e048bd26562edfac9346b086197d3829c258a
tenet <1697437561@qq.com>
2023-02-11 14:07:47 +0000

    完成信道层

--- inc/communication/channel.h
+++ inc/communication/channel.h
@@ -14,6 +14,7 @@ typedef struct comm_channel_env
 
 comm_channel_env* comm_channel_env_get_instance();
 
+/**************************************************************/
 
 // 描述一个channel的信息
 typedef struct comm_channel
@@ -26,13 +27,56 @@ typedef struct comm_channel
 // channel句柄，用户使用此对象控制对应channel
 typedef int comm_channel_handle;
 
-// 构造：分配qpair，初始化lock，初始化ref_count为0。返回非0则出错
+// 构造channel：分配qpair，初始化lock，初始化ref_count为0。返回0成功，否则返回对应errno。
 int comm_channel_constructor(comm_channel *self);
 
-// 析构：释放qpair，销毁lock
+// 析构channel：释放qpair，销毁lock
 void comm_channel_destructor(comm_channel *self);
 
+/*********************************************************************************/
 
+// 信道层channel管理器
+typedef struct comm_channel_controller
+{
+    comm_channel *channels;  // 指向分配的channel数组
+    size_t *channel_use_cnt;  // 记录每一个channel当前的使用计数
+    size_t _channel_num;  // 当前分配的channel数量
+    pthread_spinlock_t lock;  // 用于分配channel时的互斥
+} comm_channel_controller;
+
+/* 
+comm_channel_controller构造函数
+channel_num：需要分配的channel数量
+分配channels数组，并构造数组中每一个channel，然后将它们加入空闲channel链表。
+返回0成功，否则返回对应errno。
+*/
+int comm_channel_controller_constructor(comm_channel_controller *self, size_t channel_num);
+
+// 析构函数：析构每一个channel，然后释放channels数组
+void comm_channel_controller_destructor(comm_channel_controller *self);
+
+// 获取一个channel，返回该channel的句柄
+comm_channel_handle comm_channel_controller_get_channel(comm_channel_controller *self);
+
+// 释放一个channel
+void comm_channel_controller_put_channel(comm_channel_controller *self, comm_channel_handle handle);
+
+// 锁定一个channel，返回0成功，否则返回对应errno。
+int comm_channel_controller_lock_channel(comm_channel_controller *self, comm_channel_handle handle);
+
+/*
+尝试锁定一个channel。
+返回0为成功锁定；返回EBUSY为channel已被锁定；其它错误则返回对应errno。
+*/
+int comm_channel_controller_trylock_channel(comm_channel_controller *self, comm_channel_handle handle);
+
+// 解锁一个channel
+void comm_channel_controller_unlock_channel(comm_channel_controller *self, comm_channel_handle handle);
+
+/*****************************************************************************/
+/*channel handle命令操作接口*/
+
+// 信道层调用上层回调时，传递的CQE状态参数
 typedef enum CQE_status
 {
     CQE_SUCCESS, CQE_ERROR
@@ -46,43 +90,52 @@ arg是发送命令时传入的回调参数。
 */
 typedef void(*cmd_cb_func)(CQE_status status, void *arg);
 
+// 通过handle发送read命令。返回0成功，否则返回对应errno。
+int comm_channel_send_read_cmd_no_lock(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
+    cmd_cb_func cb_func, void *cb_arg);
 int comm_channel_send_read_cmd(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
     cmd_cb_func cb_func, void *cb_arg);
 
+// 通过handle发送write命令。返回0成功，否则返回对应errno。
+int comm_channel_send_write_cmd_no_lock(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
+    cmd_cb_func cb_func, void *cb_arg);
+int comm_channel_send_write_cmd(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
+    cmd_cb_func cb_func, void *cb_arg);
 
-typedef struct comm_channel_controller
+// 自定义命令的内容，由上层构造并传递给channel
+typedef struct comm_raw_cmd
 {
-    comm_channel *channels;  // 指向分配的channel数组
-    size_t *channel_use_cnt;  // 记录每一个channel当前的使用计数
-    size_t _channel_num;  // 当前分配的channel数量
-    pthread_spinlock_t lock;  // 用于分配channel时的互斥
-} comm_channel_controller;
+    uint16_t opcode;
+    uint32_t dword10;
+    uint32_t dword12;
+    uint32_t dword13;
+    uint32_t dword14;
+    uint32_t dword15;
+    uint8_t valid_bitmap;   // 暂时不用，spdk raw接口不检查有效性，无效字段可以为任意值。
+} comm_raw_cmd;
+
+#define RAW_CMD_DWORD10_VALID 1U
+#define RAW_CMD_DWORD12_VALID (1U << 1)
+#define RAW_CMD_DWORD13_VALID (1U << 2)
+#define RAW_CMD_DWORD14_VALID (1U << 3)
+#define RAW_CMD_DWORD15_VALID (1U << 4)
 
 /* 
-comm_channel_controller构造函数
-channel_num：需要分配的channel数量
-分配channels数组，并构造数组中每一个channel，然后将它们加入空闲channel链表。
-返回非0，则出错
+通过handle发送raw命令。
+上层负责构造comm_raw_cmd中的命令相关字段。
+返回0成功，否则返回对应errno。
 */
-int comm_channel_controller_constructor(comm_channel_controller *self, size_t channel_num);
-
-// 析构函数：析构每一个channel，然后释放channels数组
-void comm_channel_controller_destructor(comm_channel_controller *self);
-
-// 获取一个channel，返回该channel的句柄
-comm_channel_handle comm_channel_controller_get_channel(comm_channel_controller *self);
-
-// 释放一个channel
-void comm_channel_controller_put_channel(comm_channel_controller *self, comm_channel_handle handle);
-
-// 锁定一个channel，返回非0则出错。
-int comm_channel_controller_lock_channel(comm_channel_controller *self, comm_channel_handle handle);
+int comm_channel_send_raw_cmd_no_lock(comm_channel_handle handle, void *buf, uint32_t buf_len, 
+    comm_raw_cmd *raw_cmd, cmd_cb_func cb_func, void *cb_arg);
+int comm_channel_send_raw_cmd(comm_channel_handle handle, void *buf, uint32_t buf_len, 
+    comm_raw_cmd *raw_cmd, cmd_cb_func cb_func, void *cb_arg);
 
 /*
-尝试锁定一个channel。
-返回0为成功锁定；返回1为channel已被锁定；返回-1出错。
-*/
-int comm_channel_controller_trylock_channel(comm_channel_controller *self, comm_channel_handle handle);
+轮询handle对应channel中发送的命令是否已经完成。
+max_cplt指定此次调用最多处理已完成命令的个数，若为0则处理全部已完成命令。
+若某个命令已完成，则此函数内调用发送命令时指定的回调。
+调用前需保证已经对handle加锁。
 
-// 解锁一个channel
-void comm_channel_controller_unlock_channel(comm_channel_controller *self, comm_channel_handle handle);
+返回处理的已完成命令个数，或-ENXIO，表示底层传输出错。
+*/
+int polling_channel_completions_no_lock(comm_channel_handle handle, uint32_t max_cplt);
--- src/communication/channel.c
+++ src/communication/channel.c
@@ -2,6 +2,7 @@
 #include <error.h>
 #include "communication/channel.h"
 #include "utils/sys_log.h"
+#include "spdk/nvme_spec.h"
 
 comm_channel_env* comm_channel_env_get_instance()
 {
@@ -15,7 +16,7 @@ int comm_channel_constructor(comm_channel *self)
     if (self->qpair == NULL)
     {
         HSCFS_LOG(HSCFS_LOG_ERROR, "alloc I/O queue pair failed!");
-        return 1;
+        return ENOMEM;
     }
     int ret = pthread_mutex_init(&self->lock, NULL);
     if (ret != 0)
@@ -48,12 +49,13 @@ int comm_channel_controller_constructor(comm_channel_controller *self, size_t ch
     if (self->channels == NULL)
     {
         HSCFS_LOG(HSCFS_LOG_ERROR, "alloc channels failed!");
-        return 1;
+        return ENOMEM;
     }
     size_t init_cnt = 0;
     for (; init_cnt < channel_num; ++init_cnt)
     {
-        if (comm_channel_constructor(self->channels + init_cnt) != 0)
+        ret = comm_channel_constructor(self->channels + init_cnt);
+        if (ret != 0)
             goto err1;
     }
 
@@ -64,30 +66,29 @@ int comm_channel_controller_constructor(comm_channel_controller *self, size_t ch
         goto err1;
     }
 
-    self->channel_use_cnt = malloc(channel_num * sizeof(size_t));
+    self->channel_use_cnt = calloc(channel_num, sizeof(size_t));
     if (self->channel_use_cnt == NULL)
     {
         HSCFS_LOG(HSCFS_LOG_ERROR, "alloc channel_use_cnt failed!");
+        ret = ENOMEM;
         goto err2;
     }
-    for (size_t i = 0; i < channel_num; ++i)
-        atomic_init(&self->channel_use_cnt[i], 0);
 
     self->_channel_num = channel_num;
     return 0;
 
     // 销毁controller锁
     err2:
-    ret = pthread_spin_destroy(&self->lock);
-    if (ret != 0)
-        HSCFS_LOG_ERRNO(HSCFS_LOG_WARNING, ret, "free channel controller lock error.");
+    int res = pthread_spin_destroy(&self->lock);
+    if (res != 0)
+        HSCFS_LOG_ERRNO(HSCFS_LOG_WARNING, res, "free channel controller lock error.");
 
     // 释放已构造的所有comm_channel的资源，然后释放channels数组
     err1:
     for (size_t i = 0; i < init_cnt; ++i)
         comm_channel_destructor(self->channels + i);
     free(self->channels);
-    return 1;
+    return ret;
 }
 
 void comm_channel_controller_destructor(comm_channel_controller *self)
@@ -141,14 +142,9 @@ int comm_channel_controller_lock_channel(comm_channel_controller *self, comm_cha
 int comm_channel_controller_trylock_channel(comm_channel_controller *self, comm_channel_handle handle)
 {
     int ret = pthread_mutex_trylock(&self->channels[handle].lock);
-    if (ret == EBUSY)
-        return 1;
-    else if (ret != 0)
-    {
+    if (ret != 0 && ret != EBUSY)
         HSCFS_LOG_ERRNO(HSCFS_LOG_ERROR, ret, "trylock channel failed.");
-        return -1;
-    }
-    return 0;
+    return ret;
 }
 
 void comm_channel_controller_unlock_channel(comm_channel_controller *self, comm_channel_handle handle)
@@ -156,13 +152,15 @@ void comm_channel_controller_unlock_channel(comm_channel_controller *self, comm_
     pthread_mutex_unlock(&self->channels[handle].lock);
 }
 
-
+// 信道层使用的SPDK命令发送的回调参数
+// 包装上层调用channel发送命令时提供的回调函数和参数
 typedef struct channel_cmd_cb_ctx
 {
     cmd_cb_func caller_cb_func;
     void *caller_cb_arg;
 } channel_cmd_cb_ctx;
 
+// 信道层使用的SPDK命令发送的回调函数，封装CQE错误处理，并调用上层的回调函数，将CQE状态传入
 static void channel_inner_spdk_cmd_callback(void *ctx, const struct spdk_nvme_cpl *cpl)
 {
     channel_cmd_cb_ctx *cmd_cb_ctx = (channel_cmd_cb_ctx *)ctx;
@@ -180,44 +178,160 @@ static void channel_inner_spdk_cmd_callback(void *ctx, const struct spdk_nvme_cp
     free(cmd_cb_ctx);
 }
 
-// 应该提供加锁的/不加锁的两种接口（轮询线程不应在锁上等待，所以它只进行尝试加锁，成功后再操作，失败则等下一轮）
-int comm_channel_send_read_cmd(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
-    cmd_cb_func cb_func, void *cb_arg)
+static channel_cmd_cb_ctx *new_channel_cmd_cb_ctx(cmd_cb_func cb_func, void *cb_arg)
 {
-    int err_ret;
     channel_cmd_cb_ctx *cmd_cb_ctx = malloc(sizeof(channel_cmd_cb_ctx));
     if (cmd_cb_ctx == NULL)
     {
         HSCFS_LOG(HSCFS_LOG_ERROR, "alloc channel cmd callback ctx failed.");
-        return ENOMEM;
+        return NULL;
     }
     cmd_cb_ctx->caller_cb_func = cb_func;
     cmd_cb_ctx->caller_cb_arg = cb_arg;
+    return cmd_cb_ctx;
+}
 
+int comm_channel_send_read_cmd_no_lock(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
+    cmd_cb_func cb_func, void *cb_arg)
+{
+    channel_cmd_cb_ctx *cmd_cb_ctx = new_channel_cmd_cb_ctx(cb_func, cb_arg);
+    if (cmd_cb_ctx == NULL)
+        return ENOMEM;
+    
+    comm_channel_controller *g_channel_ctrlr = comm_channel_controller_get_instance();
+    comm_channel_env *g_env = comm_channel_env_get_instance();
+    int ret = spdk_nvme_ns_cmd_read(g_env->ns, g_channel_ctrlr->channels[handle].qpair, buffer, lba, lba_count, 
+        channel_inner_spdk_cmd_callback, cmd_cb_ctx, 0);
+    if (ret != 0)
+    {
+        ret = -ret;  // spdk返回负errno，此处将其变换为errno
+        HSCFS_LOG_ERRNO(HSCFS_LOG_ERROR, ret, "spdk send read cmd failed.");
+        free(cmd_cb_ctx);
+        return ret;
+    }
+    return 0;
+}
+
+int comm_channel_send_read_cmd(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
+    cmd_cb_func cb_func, void *cb_arg)
+{
+    int ret = 0;
     comm_channel_controller *channel_ctrlr = comm_channel_controller_get_instance();
-    err_ret = comm_channel_controller_lock_channel(channel_ctrlr, handle);
-    if (err_ret != 0)
+    ret = comm_channel_controller_lock_channel(channel_ctrlr, handle);
+    if (ret != 0)
     {
         HSCFS_LOG(HSCFS_LOG_ERROR, "send read cmd: lock channel failed.");
+        return ret;
+    }
+    ret = comm_channel_send_read_cmd_no_lock(handle, buffer, lba, lba_count, cb_func, cb_arg);
+    comm_channel_controller_unlock_channel(channel_ctrlr, handle);
+    return ret;
+}
+
+int comm_channel_send_write_cmd_no_lock(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
+    cmd_cb_func cb_func, void *cb_arg)
+{
+    channel_cmd_cb_ctx *cmd_cb_ctx = new_channel_cmd_cb_ctx(cb_func, cb_arg);
+    if (cmd_cb_ctx == NULL)
+        return ENOMEM;
+
+    comm_channel_controller *g_channel_ctrlr = comm_channel_controller_get_instance();
+    comm_channel_env *g_env = comm_channel_env_get_instance();
+    int ret = spdk_nvme_ns_cmd_write(g_env->ns, g_channel_ctrlr->channels[handle].qpair, buffer, lba, lba_count, 
+        channel_inner_spdk_cmd_callback, cmd_cb_ctx, 0);
+    if (ret != 0)
+    {
+        ret = -ret;
+        HSCFS_LOG_ERRNO(HSCFS_LOG_ERROR, ret, "spdk send write cmd failed.");
+        free(cmd_cb_ctx);
+        return ret;
+    }
+    return 0;
+}
+
+int comm_channel_send_write_cmd(comm_channel_handle handle, void *buffer, uint64_t lba, uint32_t lba_count,
+    cmd_cb_func cb_func, void *cb_arg)
+{
+    int ret = 0;
+    comm_channel_controller *channel_ctrlr = comm_channel_controller_get_instance();
+    ret = comm_channel_controller_lock_channel(channel_ctrlr, handle);
+    if (ret != 0)
+    {
+        HSCFS_LOG(HSCFS_LOG_ERROR, "send write cmd: lock channel failed.");
+        return ret;
+    }
+    ret = comm_channel_send_write_cmd_no_lock(handle, buffer, lba, lba_count, cb_func, cb_arg);
+    comm_channel_controller_unlock_channel(channel_ctrlr, handle);
+    return ret;
+}
+
+
+static void build_nvme_cmd(struct spdk_nvme_cmd *nvme_cmd, comm_raw_cmd *raw_cmd)
+{
+    nvme_cmd->opc = raw_cmd->opcode;
+    nvme_cmd->nsid = spdk_nvme_ns_get_id(comm_channel_env_get_instance()->ns);
+    nvme_cmd->cdw10 = raw_cmd->dword10;
+    nvme_cmd->cdw12 = raw_cmd->dword12;
+    nvme_cmd->cdw13 = raw_cmd->dword13;
+    nvme_cmd->cdw14 = raw_cmd->dword14;
+    nvme_cmd->cdw15 = raw_cmd->dword15;
+}
+
+int comm_channel_send_raw_cmd_no_lock(comm_channel_handle handle, void *buf, uint32_t buf_len, 
+    comm_raw_cmd *raw_cmd, cmd_cb_func cb_func, void *cb_arg)
+{
+    int ret = 0;
+    channel_cmd_cb_ctx *cmd_cb_ctx = new_channel_cmd_cb_ctx(cb_func, cb_arg);
+    if (cmd_cb_ctx == NULL)
+        return ENOMEM;
+
+    struct spdk_nvme_cmd *nvme_cmd = malloc(sizeof(struct spdk_nvme_cmd));
+    if (nvme_cmd == NULL)
+    {
+        HSCFS_LOG(HSCFS_LOG_ERROR, "alloc nvme cmd failed.");
+        ret = ENOMEM;
         goto err1;
     }
-
+    build_nvme_cmd(nvme_cmd, raw_cmd);
+    
+    comm_channel_controller *g_channel_ctrlr = comm_channel_controller_get_instance();
     comm_channel_env *g_env = comm_channel_env_get_instance();
-    err_ret = spdk_nvme_ns_cmd_read(g_env->ns, channel_ctrlr->channels[handle].qpair, buffer, lba, lba_count, 
-        channel_inner_spdk_cmd_callback, cmd_cb_ctx, 0);
-    if (err_ret != 0)
+    ret = spdk_nvme_ctrlr_cmd_admin_raw(g_channel_ctrlr, nvme_cmd, buf, buf_len, 
+        channel_inner_spdk_cmd_callback, cmd_cb_ctx);
+    if (ret != 0)
+    {
+        ret = -ret;
+        HSCFS_LOG_ERRNO(HSCFS_LOG_ERROR, ret, "spdk send raw cmd failed.");
         goto err2;
+    }
+    return ret;
 
-    comm_channel_controller_unlock_channel(channel_ctrlr, handle);
-    return 0;
-
-    // 释放channel锁
     err2:
-    comm_channel_controller_unlock_channel(channel_ctrlr, handle);
-
-    // 释放分配的cmd_cb_ctx
+    free(nvme_cmd);
     err1:
     free(cmd_cb_ctx);
+    return ret;
+}
 
-    return err_ret;
+int comm_channel_send_raw_cmd(comm_channel_handle handle, void *buf, uint32_t buf_len, 
+    comm_raw_cmd *raw_cmd, cmd_cb_func cb_func, void *cb_arg)
+{
+    int ret = 0;
+    comm_channel_controller *channel_ctrlr = comm_channel_controller_get_instance();
+    ret = comm_channel_controller_lock_channel(channel_ctrlr, handle);
+    if (ret != 0)
+    {
+        HSCFS_LOG(HSCFS_LOG_ERROR, "send raw cmd: lock channel failed.");
+        return ret;
+    }
+    ret = comm_channel_send_raw_cmd_no_lock(handle, buf, buf_len, raw_cmd, cb_func, cb_arg);
+    comm_channel_controller_unlock_channel(channel_ctrlr, handle);
+    return ret;
+}
+
+int polling_channel_completions_no_lock(comm_channel_handle handle, uint32_t max_cplt)
+{
+    comm_channel_controller *channel_ctrlr = comm_channel_controller_get_instance();
+    int ret = spdk_nvme_qpair_process_completions(channel_ctrlr->channels[handle].qpair, max_cplt);
+    return ret;
 }
