a798eb5c9ab12986ec99b9e4aee8748b06bde795
tenet <1697437561@qq.com>
2023-03-29 13:09:48 +0000

    增加通用缓存管理器与其单元测试

---
+++ inc/fs/cache_manager.hh
@@ -0,0 +1,213 @@
+#pragma once
+
+#include <unordered_map>
+#include <list>
+#include <utility>
+#include <memory>
+#include <cassert>
+
+/*
+ * 缓存索引管理器
+ * 缓存项类型为entry_t，要求缓存项具有唯一标识key(key_t类型)
+ * 当缓存项添加到索引后，由索引获得其所有权
+ */
+template <typename key_t, typename entry_t>
+class cache_hash_index
+{
+public:
+    void add(const key_t &key, std::unique_ptr<entry_t> &p_entry)
+    {
+        assert(index.count(key) == 0);
+        index[key].reset(p_entry.release());
+    }
+
+    std::unique_ptr<entry_t> remove(const key_t &key)
+    {
+        assert(index.count(key) == 1);
+        std::unique_ptr<entry_t> ret = std::move(index[key]);
+        index.erase(key);
+        return ret;
+    }
+
+    entry_t *get(const key_t &key)
+    {
+        if (index.count(key) == 0)
+            return nullptr;
+        return index[key].get();
+    }
+
+private:
+    std::unordered_map<key_t, std::unique_ptr<entry_t>> index;
+};
+
+/*
+ * 缓存lru置换器
+ * 使用缓存项的key代表缓存项
+ */
+template <typename key_t>
+class lru_replacer
+{
+public:
+    /* 
+     * 将key加入置换器
+     * 默认key可进行置换
+     */
+    void add(const key_t &key)
+    {
+        assert(key_states.count(key) == 0);
+        key_states[key].first = false;
+        add_to_list_tail(lru_list, key);
+    }
+
+    /* 返回可以被置换的缓存项数目 */
+    size_t get_num_can_replace()
+    {
+        return lru_list.size();
+    }
+
+    /* 进行置换，返回应当被置换的key */
+    key_t pop_replaced()
+    {
+        assert(lru_list.empty() == false);
+        key_t key = lru_list.front();
+        assert(key_states.at(key).first == false);
+        assert(key_states.at(key).second == lru_list.begin());
+        lru_list.pop_front();
+        key_states.erase(key);
+        return key;
+    }
+
+    /* 锁住key，让其不能被置换 */
+    void pin(const key_t &key)
+    {
+        assert(key_states.at(key).first == false);
+        auto &element = key_states[key];
+        element.first = true;
+        lru_list.erase(element.second);
+        add_to_list_tail(pinned_list, key);
+    }
+
+    /*
+     * 解锁key，使其可以被置换
+     * unpin被视为一次访问，会将其放置到lru链表尾
+     */
+    void unpin(const key_t &key)
+    {
+        assert(key_states.at(key).first == true);
+        auto &element = key_states[key];
+        element.first = false;
+        pinned_list.erase(element.second);
+        add_to_list_tail(lru_list, key);
+    }
+
+    /* 告知replacer对key进行了一次访问 */
+    void access(const key_t &key)
+    {
+        assert(key_states.count(key) == 1);
+        auto &element = key_states[key];
+
+        // 访问被pin住的缓存，不用管
+        if (element.first == true)
+            return;
+        
+        // 否则移到lru链表尾
+        lru_list.erase(element.second);
+        add_to_list_tail(lru_list, key);
+    }
+
+private:
+    /* 
+     * lru链表lru_list，不能置换的链表pinned_list
+     * lru链表中，首元素为最近最少访问的元素
+     */
+    std::list<key_t> lru_list, pinned_list;
+
+    using list_iterator_t = typename std::list<key_t>::iterator;
+
+    /* 
+     * map的value: first为当前该缓存项是否被pin住(true则被pin住)，second为list的iterator
+     * 同一时刻，根据缓存项是否被pin住，该iterator指向的元素位于lru_list(unpin)或pinned_list(pin)二者之一
+     */ 
+    std::unordered_map<key_t, std::pair<bool, list_iterator_t>> key_states;
+
+    void add_to_list_tail(std::list<key_t> &ls, const key_t &key)
+    {
+        ls.emplace_back(key);
+        key_states[key].second = --ls.end();
+    }
+};
+
+/*
+ * 通用缓存管理器
+ * 要求：
+ * 
+ * 缓存项类型为entry_t，缓存项的键的类型为key_t
+ * 
+ * 缓存索引管理器的类型为 index_t<key_t, entry_t>，能够调用：
+ * void add(const key_t &key, std::unique_ptr<entry_t> &p_entry);
+ * std::unique_ptr<entry_t> remove(const key_t &key);
+ * entry_t *get(const key_t &key);
+ * 
+ * 缓存置换器的类型为 replacer_t<key_t>，能够调用：
+ * void add(const key_t &key);
+ * size_t get_num_can_replace();
+ * key_t pop_replaced();
+ * void pin(const key_t &key);
+ * void unpin(const key_t &key);
+ * void access(const key_t &key);
+ * 
+ */
+template <typename key_t, typename entry_t, 
+    template <typename, typename> class index_t, 
+    template <typename> class replacer_t>
+class generic_cache_manager
+{
+public:
+    /* 增加缓存项p_entry，并把该缓存项的所有权交给cache_manager */
+    void add(const key_t &key, std::unique_ptr<entry_t> &p_entry)
+    {
+        index.add(key, p_entry);
+        replacer.add(key);
+    }
+
+    /* 
+     * 获取键为key的缓存项，若不存在返回nullptr
+     * 视为对key的一次访问
+     * 调用者不拥有该缓存项指针的所有权，不能释放 
+     */
+    entry_t *get(const key_t &key)
+    {
+        entry_t *ret = index.get(key);
+        if (ret != nullptr)
+            replacer.access(key);
+        return ret;
+    }
+
+    /* pin住缓存项，标识其不能被置换 */
+    void pin(const key_t &key)
+    {
+        replacer.pin(key);
+    }
+
+    /* unpin缓存项，使其可以被置换 */
+    void unpin(const key_t &key)
+    {
+        replacer.unpin(key);
+    }
+
+    /*
+     * 置换一个缓存项，将其从cache_manager中移除并获取其所有权
+     * 若没有可置换的缓存项，返回nullptr
+     */
+    std::unique_ptr<entry_t> replace_one()
+    {
+        if (replacer.get_num_can_replace() == 0)
+            return nullptr;
+        key_t key = replacer.pop_replaced();
+        return index.remove(key);
+    }
+
+private:
+    index_t<key_t, entry_t> index;  // 缓存索引
+    replacer_t<key_t> replacer;  // 置换器
+};
---
+++ test/fs/test_cache_manager.cc
@@ -0,0 +1,104 @@
+#include "fs/cache_manager.hh"
+#include "gtest/gtest.h"
+
+#include <vector>
+
+using std::unique_ptr;
+using std::vector;
+
+struct cache_obj
+{
+    int k, v;
+    cache_obj(int k_ = 0, int v_ = 0) {
+        k = k_;
+        v = v_;
+    }
+};
+
+class test_cache_manager: public ::testing::Test
+{
+protected:
+    using cache_manager_t = generic_cache_manager<int, cache_obj, cache_hash_index, lru_replacer>;
+
+    void SetUp() override
+    {
+        cache_manager.reset(new cache_manager_t);
+    }
+
+protected:
+    unique_ptr<cache_manager_t> cache_manager;
+};
+
+TEST_F(test_cache_manager, basic_function)
+{
+    unique_ptr<cache_obj> p1(new cache_obj);
+    int test_key = 1, test_value = 10;
+    p1->k = test_key;
+    p1->v = test_value;
+    cache_manager->add(p1->k, p1);
+    auto p2 = cache_manager->get(test_key);
+    EXPECT_EQ(p2->v, test_value);
+    auto p3 = cache_manager->get(2);
+    EXPECT_EQ(p3, nullptr);
+    cache_manager->pin(test_key);
+    auto p4 = cache_manager->replace_one();
+    EXPECT_EQ(p4, nullptr);
+    cache_manager->unpin(test_key);
+    auto p5 = cache_manager->replace_one();
+    EXPECT_EQ(p5->k, test_key);
+    EXPECT_EQ(p5->v, test_value);
+}
+
+TEST_F(test_cache_manager, lru)
+{
+    vector<unique_ptr<cache_obj>> v;
+    for (int i = 0; i < 10; ++i)
+        v.emplace_back(std::make_unique<cache_obj>(i, i));
+    for (int i = 0; i < 10; ++i)
+        cache_manager->add(i, v[i]);
+    for (int i = 9; i >= 0; --i)
+        cache_manager->get(i);
+    for (int i = 0; i < 10; ++i)
+    {
+        auto p = cache_manager->replace_one();
+        EXPECT_EQ(p->k, 9 - i);
+    }
+}
+
+TEST_F(test_cache_manager, lru_with_pin)
+{
+    vector<unique_ptr<cache_obj>> v;
+    for (int i = 0; i < 10; ++i)
+        v.emplace_back(std::make_unique<cache_obj>(i, i));
+    for (int i = 0; i < 10; ++i)
+        cache_manager->add(i, v[i]);
+    for (int i = 0; i < 10; ++i)
+    {
+        cache_manager->get(i);
+        if (i & 1)
+            cache_manager->pin(i);
+    }
+    for (int i = 0; ; i += 2)
+    {
+        unique_ptr<cache_obj> p = cache_manager->replace_one();
+        if (p == nullptr)
+        {
+            EXPECT_EQ(i, 10);
+            break;
+        }
+        EXPECT_EQ(p->k, i);
+    }
+    for (int i = 1; i < 10; i += 2)
+        cache_manager->unpin(i);
+    for (int i = 1; i < 10; i += 2)
+    {
+        unique_ptr<cache_obj> p = cache_manager->replace_one();
+        EXPECT_EQ(p->k, i);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
