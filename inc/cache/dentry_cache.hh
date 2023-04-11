5c0fa6ad8b349129b0718e643c3b1d5d6a0b3610
tenet <1697437561@qq.com>
2023-04-11 14:40:20 +0000

    完成部分dentry cache

--- inc/cache/dentry_cache.hh
+++ inc/cache/dentry_cache.hh
@@ -32,12 +32,28 @@ struct hash<hscfs::dentry_key>
 
 namespace hscfs {
 
+class super_manager;
+
+enum class dentry_state
+{
+    valid,
+    deleted_inode_valid,
+    deleted
+};
+
 class dentry
 {
 public:
     dentry_key key;
-    uint32_t ino;   // 目录项的inode
-    /* to do */
+    uint32_t ino;  // 目录项的inode
+    super_manager *super;
+    dentry *parent;  // 目录树中的父目录，根目录为nullptr
+
+    uint32_t blkno, slotno;  // 目录项在目录文件中的位置(块号，块内slot号)
+    bool is_dentry_pos_valid;  // blkno和slotno字段是否有效(由SSD path lookup返回的中间节点则不一定有效)
+
+    uint32_t ref_count;
+    dentry_state state;
 };
 
 class dentry_handle
--- inc/fs/super_manager.hh
+++ inc/fs/super_manager.hh
@@ -1,11 +1,9 @@
 #pragma once
 
-
-
 namespace hscfs {
 
 /* 类似VFS的struct super block中的文件系统私有信息 */
-class fs_manager
+class super_manager
 {
 public:
 
